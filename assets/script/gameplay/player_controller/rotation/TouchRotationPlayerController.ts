d414ee854e2ea0119018f63b16e7302167c0722c
horchynskyi <horchynskyi@gmail.com>
2025-09-06 13:36:44 +0300

    Fixed touch rotation.

---
+++ assets/script/gameplay/player_controller/rotation/TouchRotationPlayerController.ts
@@ -0,0 +1,45 @@
+import {
+	_decorator,
+	EventMouse,
+	EventTouch,
+	input,
+	Input,
+	math,
+	Vec2,
+} from 'cc';
+import { BasicRotationPlayerController } from './BasicRotationPlayerController';
+import { HALF_WINDOW_SIZE } from '../../constants';
+const { ccclass, property } = _decorator;
+
+@ccclass('TouchRotationPlayerController')
+export class TouchRotationPlayerController extends BasicRotationPlayerController {
+	protected tempVec2: Vec2 = new Vec2();
+
+	protected onLoad(): void {
+		input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+		input.on(Input.EventType.TOUCH_START, this.onTouchMove, this);
+	}
+
+	protected onTouchStart(event: EventTouch) {
+		this.changeRotation(event);
+	}
+
+	protected onTouchMove(event: EventTouch) {
+		this.changeRotation(event);
+	}
+
+	protected changeRotation(event: EventTouch) {
+		event.getLocation(this.tempVec2);
+
+		const halfScreenSize = new Vec2(HALF_WINDOW_SIZE.x, HALF_WINDOW_SIZE.y);
+
+		this.rotation = math.toDegree(
+			Math.atan2(
+				this.tempVec2.y - halfScreenSize.y,
+				this.tempVec2.x - halfScreenSize.x
+			)
+		);
+
+		this.emitChangeRotation();
+	}
+}
